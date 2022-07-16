eb8fc6718fa22f4f47b4903d1e6fcf73d8c53cbd
КириллПятаков <kpyatakov@gmail.com>
2022-07-16 22:31:09 +0700

    добавлены тесты для незарегистрированных

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,5 +9,13 @@ public class Main {
         expected = 500;
         actual = service.calculate(1_000_000, true);
         System.out.println("1. " + expected + " == ? == " + actual);
+
+        expected = 10;
+        actual = service.calculate(1000, false);
+        System.out.println("1. " + expected + " == ? == " + actual);
+
+        expected = 500;
+        actual = service.calculate(1_000_000, false);
+        System.out.println("1. " + expected + " == ? == " + actual);
     }
 }
